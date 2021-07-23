2a5ab7a1790f7b6b38649f0d9d54833de5001b04
williamcostaramos <wulliamcostaramos@gmail.com>
2021-07-23 01:10:28 -0300

    selecionando dom com o metodo getElementById()

---
+++ dom/selecionando-elemento-dom.js
@@ -0,0 +1,3 @@
+// selecionando elemento pelo id
+  document.getElementById('exemplo');
+console.log(document.getElementById('exemplo')); // caso seu codigo não esteja funcioando verifique no seu html se seu script esta dentro da tag <body></body>
